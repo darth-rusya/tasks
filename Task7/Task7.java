a1fe6b8f92110cc5d80609fe923e0df2da48ddfd
Rustem <rustem.karimov.2002@gmail.com>
2020-09-22 21:28:50 +0300

    fixed Task7.java

--- Task7/Task7.java
+++ Task7/Task7.java
@@ -1,22 +1,28 @@
-public class rusya{
+public class Task7{
 
 	public static void main(String[] args){
-		double result;
-		int Number = Integer.valueOf(args[0]);
-		int present = 1;
-		int head_number = 1;
-		result = 0;
-		while(Number+1!=present){
-			if (present%2!=0){
-				result=result+1/Math.pow(head_number,2);
+		if (args.length == 1){
+			int Number = Integer.valueOf(args[0]);
+			if (Number>0){
+				int present = 1;
+				int head_number = 1;
+				double result = 0.0d;
+				while(Number+1!=present){
+					if (present%2!=0){
+						result=result+1/Math.pow(head_number,2);
+					}
+					if (present%2==0){
+						result=result-1/Math.pow(head_number,2);
+					}
+					head_number += 2;
+					present += 1;
+				}
+				System.out.println("Result " + result);
+			} else {
+				System.out.println("Error");
 			}
-			if (present%2==0){
-				result=result-1/Math.pow(head_number,2);
-			}
-			head_number += 2;
-			present += 1;
+		} else {
+			System.out.println("List index out of range");
 		}
-		System.out.println("Результат " + result);
 	}
-
 }
