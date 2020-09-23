4ec341970c0f30c10f809c0166a40dba82f29bbe
Rustem <rustem.karimov.2002@gmail.com>
2020-09-23 15:55:38 +0300

    fixed task3

--- task3/Task3.java
+++ task3/Task3.java
@@ -1,12 +1,16 @@
 public class Task3 {
 
 	public static void main(String[] args){
-		double result = 1;
-		int N = Integer.valueOf(args[0]);
-		for (int i=1; i<=N; i++) {
-		    double Number = Math.pow(i*2,2);
-		    result *= Number/(Number-1);
-		}
-		System.out.println(result*2);
+		if (args.length==1){
+			int N = Integer.valueOf(args[0]);
+			if (N>0){
+				double result = 1;
+				for (int i=1; i<=N; i++) {
+			    	double Number = Math.pow(i*2,2);
+			    	result *= Number/(Number-1);
+				}
+				System.out.println(result*2);
+			}
+		}	
 	}
 }
