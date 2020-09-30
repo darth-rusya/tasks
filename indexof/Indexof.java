4a264b71079db43c302ef6268f4b6b56f1a9c1c3
Rustem <rustem.karimov.2002@gmail.com>
2020-09-30 16:11:33 +0300

    modified Indexof.java

--- indexof/Indexof.java
+++ indexof/Indexof.java
@@ -3,21 +3,29 @@ public class Indexof{
 	public static void main(String[] args){
 		String string = args[0];
 		String substring = args[1];
-		int n = 0;
-		for (int i = 0; i < string.length(); i++){
-			if (string.charAt(i)==substring.charAt(n)){
-				n++;
+		if (string.length()!=0){
+			if (substring.length()!=0){
+				int n = 0;
+				for (int i = 0; i < string.length(); i++){
+					if (string.charAt(i)==substring.charAt(n)){
+						n++;
+					} else {
+						n = 0;
+					}
+					if (n==substring.length()){
+						i-=n-1;
+						System.out.println(i);
+						break;
+					}
+				}
+				if (n==0){
+					System.out.println(-1);
+				}
 			} else {
-				n = 0;
+				System.out.println("Incorrect length of substring");
 			}
-			if (n==substring.length()){
-				i-=n-1;
-				System.out.println(i);
-				break;
-			}
-		}
-		if (n==0){
-			System.out.println(-1);
+		} else {
+			System.out.println("Incorrect length of string");
 		}
 	}
 }
